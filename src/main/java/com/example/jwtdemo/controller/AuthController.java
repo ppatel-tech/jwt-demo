60b428f7c27e8574b026e6ac7a3111390bc92d4b
purushottam patel <purushottampatel.tech@gmail.com>
2026-05-10 16:51:12 +0530

    refectored
    Cleaned up formatting by removing extra blank lines.

--- src/main/java/com/example/jwtdemo/controller/AuthController.java
+++ src/main/java/com/example/jwtdemo/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.example.jwtdemo.controller;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.jwtdemo.entity.User;
 import com.example.jwtdemo.service.UserService;
 import com.example.jwtdemo.util.JwtUtil;
